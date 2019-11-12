da0ebea2770f070cac2d61f070cc832e46432e37
mateusz <mateusz@gmail.com>
2019-11-12 20:37:33 +0100

    znajdz_blad1 solved

--- zadania_domowe/zadania_2/znajdz_blad1.py
+++ zadania_domowe/zadania_2/znajdz_blad1.py
@@ -14,7 +14,15 @@
 
 def compute_the_lowest_value(numbers):
     ans = 0
-    for i in range(1, len(numbers)):
+    for i in range(len(numbers)):
         if numbers[i] < ans:
             ans = numbers[i]
     return ans
+
+
+if __name__ == '__main__':
+    a = [4, 2, 0, 5, 11, 9, 2]
+    b = [-5, 3, 2, -1, 9]
+    c = [1, 6, -2, 8, 10, -3]
+
+    print(compute_the_lowest_value(b))
